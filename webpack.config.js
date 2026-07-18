f9bc3af73ded0e8a0faa474714b121ddf8636c0f
Subbu <rsubbu931@gmail.com>
2026-07-18 14:31:05 +0530

    Set up the required modules, pulgins, loaders

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+import path from "node:path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import { watchFile } from "node:fs";
+
+export default {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(import.meta.dirname, "dist"),
+        clean: true,
+    },
+    devtool: "eval-source-map",
+    devServer: {
+        watchFiles: ["./src/index.html"]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(jpg|png|jpeg|svg)$/i,
+                use: "asset/resource",
+            },  
+        ]
+    }
+};
