33ceee15f8130cc5741a54787306e8e91ba3ccc2
Subbu <rsubbu931@gmail.com>
2026-07-19 18:01:35 +0530

    Complete remaining contact-page with dummy data

--- src/contact-page.js
+++ src/contact-page.js
@@ -1,11 +1,14 @@
 export function contactPage() {
     return `
-        <div class="contact-content">
+       <div class="contact-content">
 			<div class="heading">
 				<p>Want to know more??</p>
-				<p>Find Us Here</p>
 			</div>
-			
+			<div class="contact-details">
+				<div class="instagram">worldno_1restaurant</div>
+				<div class="email">worldno_1restaurant@gmail.com</div>
+				<div class="x">worldno_1restaurant@x account</div>
+			</div>
 		</div>
     `;
 }
